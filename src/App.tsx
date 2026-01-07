f42bd6a0d227a4945bc9b538b04363296e50411b
slarz <bourdinflorent86@gmail.com>
2026-01-07 12:51:18 +0100

    Fix: Ajout de Header et Footer dans App en les enlevant de AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import AppRouter from "@/router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 function App() {
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <AppRouter />
+      <Footer footerText="Copyright 2020 Argent Bank" />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,22 +1,15 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Home from "@/pages/Home";
 import SignIn from "@/pages/SignIn";
 import User from "@/pages/User";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
-      </Routes>
-      <Footer footerText="Copyright 2020 Argent Bank" />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/user" element={<User />} />
+    </Routes>
   );
 };
 
